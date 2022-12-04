d2808c3716032d9a5fa4111b785df095dba3a266
jeonhyoungmin <jhmin7509@gamil.com>
2022-12-05 01:43:42 +0900

    변수 값 객체 모음

---
+++ frontend/src/value.js
@@ -0,0 +1,68 @@
+const cameraValue = {
+  fov: 60,
+  near: 0.1,
+  far: 10000,
+  positionX: 0,
+  positionY: 200,
+  positionZ: -500,
+};
+
+const ambientLightValue = {
+  on: true,
+  color: 0xffffff,
+  intensity: 1,
+};
+
+const shadowLightValue = {
+  on: true,
+  color: 0xffffff,
+  intensity: 1,
+  positionX: -2000,
+  positionY: 2000,
+  positionZ: 1000,
+  targetX: -1000,
+  targetY: 0,
+  targetZ: -2000,
+  castShadow: true,
+  width: 2000,
+  height: 1000,
+  TopRight: 5000,
+  BottomLeft: -3000,
+  near: 10,
+  far: 3500,
+  radius: 5,
+  helperSize: 10,
+};
+
+const addPointLightValue = {
+  on: false,
+  color: 0xffffff,
+  intensity: 1.5,
+  distance: 1000,
+  sphereSize: 10,
+};
+
+const spaceValue = {
+  positionX: 0,
+  positionY: 0,
+  positionZ: 0,
+};
+
+const characterValue = {
+  positionX: 0,
+  positionY: 0,
+  positionZ: 0,
+  capsuleStart: 90.9 / 2,
+  capsuleEnd: 259 - 90.9 / 2,
+  capsuleRadius: 90.9 / 2,
+  axisHelper: 10000,
+};
+
+export {
+  cameraValue,
+  ambientLightValue,
+  shadowLightValue,
+  addPointLightValue,
+  spaceValue,
+  characterValue,
+};
