12a82457c35c3ce1003871042896cbe23355c0b9
jeonhyoungmin <jhmin7509@gamil.com>
2023-02-09 20:23:48 +0900

    ugauga 소개 내용물

---
+++ src/ugauga_pr/ugauga_pr_contents.js
@@ -0,0 +1,31 @@
+import '../../public/img/ugauga_search.png';
+import '../../public/img/ugauga_loading.png';
+import '../../public/img/ugauga_recommendation.png';
+import '../../public/img/ugauga_news.png';
+import '../../public/img/ugauga_park.png';
+import '../../public/img/ugauga_jeong.png';
+import '../../public/img/ugauga_kim.png';
+import '../../public/img/ugauga_jeon.png';
+import '../../public/img/ugauga_kang.png';
+
+const contents = [
+  ['../../public/img/ugauga_search.png', 'Search', 'Park', '상세 검색'],
+  ['../../public/img/ugauga_loading.png', 'Loading', 'Jeong', '로딩창'],
+  [
+    '../../public/img/ugauga_recommendation.png',
+    'Recommendation',
+    'Kim',
+    '주식 추천',
+  ],
+  ['../../public/img/ugauga_news.png', 'News', 'Jeon', '동향 뉴스'],
+];
+
+const team_member = [
+  ['Park', 'Search', 'team_member', '../../public/img/ugauga_park.png'],
+  ['Jeong', 'Loading', 'team_member', '../../public/img/ugauga_jeong.png'],
+  ['Kim', 'recommendation', 'team_leader', '../../public/img/ugauga_kim.png'],
+  ['Jeon', 'News', 'team_member', '../../public/img/ugauga_jeon.png'],
+  ['Kang', 'Markup', 'team_member', '../../public/img/ugauga_kang.png'],
+];
+
+export { contents, team_member };
